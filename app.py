5347066759a4225238e09cc2c0e292cbbbfa7fc1
ramy <ramygharib91@gmail.com>
2021-03-08 01:41:20 +0200

    prices_app_flask_v_2.1.1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from scripts_py import py_get_data
 from scripts_py import search_online
-from scripts_py import db_search
+# from scripts_py import db_search
 
 app = Flask(__name__)
 
@@ -36,7 +36,7 @@ def search():
 def search_data(search_value):
     if request.method == 'GET':
         # search_value = request.form.get("search_value")
-        data = db_search.db_connection(search_value)
+        data = ''
         # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
         return render_template('search.html', data=data)
     else:
