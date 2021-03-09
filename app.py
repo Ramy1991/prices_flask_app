bd3e24a140bd79ae1e4b8b647cc0584d97041f1f
ramy <ramygharib91@gmail.com>
2021-03-09 03:27:43 +0200

    prices_app_flask_v_2.1.1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from scripts_py import py_get_data
 from scripts_py import search_online
-# from scripts_py import db_search
+from scripts_py import db_search
 
 app = Flask(__name__)
 
@@ -32,17 +32,16 @@ def get_item_data():
 @app.route('/search/')
 def search():
     return render_template('search.html', data='')
-#
-#
-# @app.route('/search/<search_value>')
-# def search_data(search_value):
-#     if search_value:
-#         # search_value = request.form.get("search_value")
-#         data = db_search.db_connection(search_value)
-#         # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
-#         return render_template('search.html', data=data)
-#     else:
-#         return render_template('search.html')
-#
-#
+
+
+@app.route('/search/<search_value>')
+def search_data(search_value):
+    if search_value:
+        # search_value = request.form.get("search_value")
+        data = db_search.db_connection(search_value)
+        # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
+        return render_template('search.html', data=data)
+    else:
+        return render_template('search.html')
+
 # app.run(debug=True, port=5000)
