5018cd2a16294ee7f546da174ab0adcf4abd86d1
ramy <ramygharib91@gmail.com>
2021-03-09 03:04:04 +0200

    prices_app_flask_v_2.1.1

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, render_template
-# from scripts_py import py_get_data
-# from scripts_py import search_online
-# from scripts_py import db_search
+from scripts_py import py_get_data
+from scripts_py import search_online
+from scripts_py import db_search
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
+@app.route('/home/')
 def index():
-    return 'hello'
+    return render_template('home.html')
 
 
 @app.route('/user/<string:name>')
@@ -21,26 +22,27 @@ def user(name):
 def get_item_data():
     if request.method == 'POST':
         item_data = request.form.get("name")
-        # responses = py_get_data.check_url(item_data)
-        return 'responses'
+        responses = py_get_data.check_url(item_data)
+        return responses
     else:
         return render_template('user.html')
 
 
 @app.route('/search')
+@app.route('/search/')
 def search():
     return render_template('search.html', data='')
 
 
-@app.route('/search/<search_value>', methods=['GET', 'POST'])
+@app.route('/search/<search_value>')
 def search_data(search_value):
-    if request.method == 'GET':
+    if search_value:
         # search_value = request.form.get("search_value")
-        data = ''
+        data = db_search.db_connection(search_value)
         # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
         return render_template('search.html', data=data)
     else:
         return render_template('search.html')
 
 
-# app.run(debug=True, port=5000)
+app.run(debug=True, port=5000)
