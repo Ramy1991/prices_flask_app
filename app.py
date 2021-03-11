1ace2fc2f438b2aa9806c13c53ebec37f3ec4044
ramy <ramygharib91@gmail.com>
2021-03-11 02:48:39 +0200

    prices_app_flask_v_2.1.1

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, render_template
 from scripts_py import py_get_data
-
 # from scripts_py import search_online
-# from scripts_py import db_search
+from scripts_py import db_search
 
 app = Flask(__name__)
 
@@ -19,38 +18,36 @@ def user(name):
     return render_template('user.html', username=name)
 
 
-# @app.route('/search', methods=['GET', 'POST'])
-# def search():
-#     return render_template('search.html', data='')
-
 @app.route('/search')
 @app.route('/search/')
 def search():
-    return "default"
+    return render_template('search.html', data='')
 
 
 @app.route('/search/<search_value>')
 def search_data(search_value):
-    # if search_value:
-    #     # search_value = request.form.get("search_value")
-    #     data = db_search.db_connection(search_value)
-    #     # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
-    #     return render_template('search.html', data=data)
-    # else:
     if search_value:
-        return search_value
+        # search_value = request.form.get("search_value")
+        data = db_search.db_connection(search_value)
+        # data = search_online.main(search_online.create_url(search_value, 'Egypt'))
+        return render_template('search.html', data=data)
     else:
-        return "default"
+        return render_template('search.html', data='')
+    # if search_value:
+    #     return search_value
+    # else:
+    #     return "default"
 
 
 #
-# @app.route('/get_item_data', methods=['GET', 'POST'])
-# def get_item_data():
-#     if request.method == 'POST':
-#         item_data = request.form.get("name")
-#         responses = py_get_data.check_url(item_data)
-#         return responses
-#     else:
-#         return render_template('user.html')
+@app.route('/get_item_data', methods=['GET', 'POST'])
+def get_item_data():
+    if request.method == 'POST':
+        item_data = request.form.get("name")
+        responses = py_get_data.check_url(item_data)
+        return responses
+    else:
+        return render_template('user.html')
 
-# app.run(debug=True, port=5000)
+
+app.run(debug=True, port=5000)
