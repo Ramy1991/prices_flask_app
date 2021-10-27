ab59cef3f5671b861c7604070d8ac8d34339dab9
ramy <ramygharib91@gmail.com>
2021-10-27 19:52:09 +0200

    1.2.2

--- app.py
+++ app.py
@@ -156,6 +156,6 @@ def about():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0")
-    app.run(debug=True, port=5000, ssl_context='adhoc')
+    app.run(host="0.0.0.0")
+    # app.run(debug=True, port=5000, ssl_context='adhoc')
     # app.run()
