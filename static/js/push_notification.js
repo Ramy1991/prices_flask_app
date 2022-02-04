245102741a215de264efac4e2f7da4e28001ddef
ramy <ramygharib91@gmail.com>
2022-02-05 01:25:39 +0200

    1.2.3

--- static/js/push_notification.js
+++ static/js/push_notification.js
@@ -53,7 +53,7 @@ function subscribe() {
     const subscribeOptions = {
       userVisibleOnly: true,
       applicationServerKey: 
-        'AAAA7CWbgLU:APA91bECBRN1NDe9l7QBa--1pd69nNOKrIJdIm6FRXo793JsOXvfcijyc_KJlOv34DggcHeS9jX4As0r278Qne4QyQ4aXh9E9EhUhLZmJYpWMGm3vMH4LweFgP0JDtauovQivzV8nk8B'
+      window.atob('AAAA7CWbgLU:APA91bECBRN1NDe9l7QBa--1pd69nNOKrIJdIm6FRXo793JsOXvfcijyc_KJlOv34DggcHeS9jX4As0r278Qne4QyQ4aXh9E9EhUhLZmJYpWMGm3vMH4LweFgP0JDtauovQivzV8nk8B')
     };
     serviceWorkerRegistration.pushManager.subscribe(subscribeOptions)
       .then(function(subscription) {
