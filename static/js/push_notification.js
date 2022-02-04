0f4d82b93181b5cf0ade291010db93a0775f56ed
ramy <ramygharib91@gmail.com>
2022-02-05 00:34:21 +0200

    1.2.2

--- static/js/push_notification.js
+++ static/js/push_notification.js
@@ -13,207 +13,48 @@ const firebaseConfig = {
     measurementId: "G-275CEDTFV7"
 };
 
-
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(window.location.origin + "/firebase-messaging-sw.js", {scope: '/'}).then(initialiseState);
-} else {
-  console.warn('Service workers aren\'t supported in this browser.');
-}
-
-function initialiseState() {
-  // Are Notifications supported in the service worker?
-  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
-    console.warn('Notifications aren\'t supported.');
-    return;
+    navigator.serviceWorker.register(window.location.origin + "/firebase-messaging-sw.js", {scope: '/'}).then(registration => {
+        console.log("ServiceWorker running");
+    }).catch(err => {
+        console.log(err);
+    })
   }
 
-  // Check the current Notification permission.
-  // If its denied, it's a permanent block until the
-  // user changes the permission
-  if (Notification.permission === 'denied') {
-    console.warn('The user has blocked notifications.');
-    return;
-  }
-
-  // Check if push messaging is supported
-  if (!('PushManager' in window)) {
-    console.warn('Push messaging isn\'t supported.');
-    return;
-  }
-
-  // We need the service worker registration to check for a subscription
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // Do we already have a push message subscription?
-    serviceWorkerRegistration.pushManager.getSubscription()
-      .then(function(subscription) {
-        // Enable any UI which subscribes / unsubscribes from
-        // push messages.
-        var pushButton = document.querySelector('.js-push-button');
-        pushButton.disabled = false;
-
-        if (!subscription) {
-          // We aren't subscribed to push, so set UI
-          // to allow the user to enable push
-          return;
-        }
-
-        // Keep your server in sync with the latest subscriptionId
-        sendSubscriptionToServer(subscription);
-
-        // Set your UI to show they have subscribed for
-        // push messages
-        pushButton.textContent = 'Disable Push Messages';
-        isPushEnabled = true;
-      })
-      .catch(function(err) {
-        console.warn('Error during getSubscription()', err);
-      });
-  });
-}
 
 
-  // ASK FOR PUSH NOTIFICATION APPROVAL
-
-  
-
-function askForApproval() {
-  if(Notification.permission === "granted") {
-    createNotification('Wow! This is great', 
-                       'created by @study.tonight', 
-                       'https://www.studytonight.com/css/resource.v2/icons/studytonight/st-icon-dark.png');
-    subscribe();
-                                          
+const app = initializeApp(firebaseConfig);
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: 'BCCQ90gHgbcJsHwGMOFcA7ZleVGWn8VRvvfiQ7_kXkQjKCtbVItTZ_jWclGCWdRvWiv9wVUnVEZu6dSSwt9A13s' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    console.log(currentToken);
+    // ...
   } else {
-    
-    Notification.requestPermission(permission => {
-      if(permission === 'granted') {
-        createNotification('Wow! This is great', 'created by @study.tonight', 'https://www.studytonight.com/css/resource.v2/icons/studytonight/st-icon-dark.png');
-      }
-    });
-    unsubscribe();
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
   }
-}
-
-function createNotification(title, text, icon) {
-  const noti = new Notification(title, {
-    body: text,
-    icon
-  });
-}
-askForApproval();
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
 
 
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
 
-function subscribe() {
-  // Disable the button so it can't be changed while
-  // we process the permission request
-  var pushButton = document.querySelector('.js-push-button');
-  pushButton.disabled = true;
-
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    serviceWorkerRegistration.pushManager.subscribe()
-      .then(function(subscription) {
-        // The subscription was successful
-        isPushEnabled = true;
-        pushButton.textContent = 'Disable Push Messages';
-        pushButton.disabled = false;
-
-        // TODO: Send the subscription.endpoint to your server
-        // and save it to send a push message at a later date
-        return sendSubscriptionToServer(subscription);
-      })
-      .catch(function(e) {
-        if (Notification.permission === 'denied') {
-          // The user denied the notification permission which
-          // means we failed to subscribe and the user will need
-          // to manually change the notification permission to
-          // subscribe to push messages
-          console.warn('Permission for Notifications was denied');
-          pushButton.disabled = true;
-        } else {
-          // A problem occurred with the subscription; common reasons
-          // include network errors, and lacking gcm_sender_id and/or
-          // gcm_user_visible_only in the manifest.
-          console.error('Unable to subscribe to push.', e);
-          pushButton.disabled = false;
-          pushButton.textContent = 'Enable Push Messages';
-        }
-      });
-  });
-}
-
-
-function unsubscribe() {
-  var pushButton = document.querySelector('.js-push-button');
-  pushButton.disabled = true;
-
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // To unsubscribe from push messaging, you need get the
-    // subscription object, which you can call unsubscribe() on.
-    serviceWorkerRegistration.pushManager.getSubscription().then(
-      function(pushSubscription) {
-        // Check we have a subscription to unsubscribe
-        if (!pushSubscription) {
-          // No subscription object, so set the state
-          // to allow the user to subscribe to push
-          isPushEnabled = false;
-          pushButton.disabled = false;
-          pushButton.textContent = 'Enable Push Messages';
-          return;
-        }
-
-        var subscriptionId = pushSubscription.subscriptionId;
-        // TODO: Make a request to your server to remove
-        // the subscriptionId from your data store so you
-        // don't attempt to send them push messages anymore
-
-        // We have a subscription, so call unsubscribe on it
-        pushSubscription.unsubscribe().then(function(successful) {
-          pushButton.disabled = false;
-          pushButton.textContent = 'Enable Push Messages';
-          isPushEnabled = false;
-        }).catch(function(e) {
-          // We failed to unsubscribe, this can lead to
-          // an unusual state, so may be best to remove
-          // the users data from your data store and
-          // inform the user that you have done so
-
-          console.log('Unsubscription error: ', e);
-          pushButton.disabled = false;
-          pushButton.textContent = 'Enable Push Messages';
-        });
-      }).catch(function(e) {
-        console.error('Error thrown while unsubscribing from push messaging.', e);
-      });
-  });
-}
-
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register(window.location.origin + "/firebase-messaging-sw.js", {scope: '/'}).then(registration => {
-//         console.log("ServiceWorker running");
-//     }).catch(err => {
-//         console.log(err);
-//     })
-//   }
-
-
-
-// const app = initializeApp(firebaseConfig);
-// // Get registration token. Initially this makes a network call, once retrieved
-// // subsequent calls to getToken will return from cache.
-// const messaging = getMessaging();
-// getToken(messaging, { vapidKey: 'BCCQ90gHgbcJsHwGMOFcA7ZleVGWn8VRvvfiQ7_kXkQjKCtbVItTZ_jWclGCWdRvWiv9wVUnVEZu6dSSwt9A13s' }).then((currentToken) => {
-//   if (currentToken) {
-//     // Send the token to your server and update the UI if necessary
-//     console.log(currentToken);
-//     // ...
-//   } else {
-//     // Show permission request UI
-//     console.log('No registration token available. Request permission to generate one.');
-//     // ...
-//   }
-// }).catch((err) => {
-//   console.log('An error occurred while retrieving token. ', err);
-//   // ...
-// });
+  var title = data.notification.title;
+  var body = data.notification.message;
+  var icon = data.notification.icon;
+  var notificationTag = data.notification.tag;
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: notificationTag
+    })
+  );
+});
