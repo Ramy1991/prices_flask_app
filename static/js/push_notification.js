1d4932facb45bbee72fc0d376bcac35b587e68ae
ramy <ramygharib91@gmail.com>
2022-02-05 22:50:45 +0200

    1.2.3

--- static/js/push_notification.js
+++ static/js/push_notification.js
@@ -1,4 +1,4 @@
-import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging.js';
+import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 
 
@@ -13,10 +13,12 @@ const firebaseConfig = {
     measurementId: "G-275CEDTFV7"
 };
 
+
+
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(window.location.origin + "/firebase-messaging-sw.js", {scope: '/'}).then(registration => {
         console.log("ServiceWorker running");
-        subscribe();
     }).catch(err => {
         console.log(err);
     })
@@ -44,87 +46,93 @@ getToken(messaging, { vapidKey: 'BCCQ90gHgbcJsHwGMOFcA7ZleVGWn8VRvvfiQ7_kXkQjKCt
   // ...
 });
 
-function subscribe() {
-  // Disable the button so it can't be changed while
-  // we process the permission request
-  // var pushButton = document.querySelector('.js-push-button');
-  // pushButton.disabled = true;
-
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-    // const pushSubscription = serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true});
-    // The push subscription details needed by the application
-    // server are now available, and can be sent to it using,
-    // for example, an XMLHttpRequest.
-    // console.log(pushSubscription);
-    // console.log(pushSubscription.getKey("p256dh"));
-    // console.log(pushSubscription.getKey("auth"));
-    const subscribeOptions = {
-      userVisibleOnly: true,
-      applicationServerKey: convertDataURIToBinary('BCCQ90gHgbcJsHwGMOFcA7ZleVGWn8VRvvfiQ7_kXkQjKCtbVItTZ_jWclGCWdRvWiv9wVUnVEZu6dSSwt9A13s')
-    };
-    serviceWorkerRegistration.pushManager.subscribe(subscribeOptions).then(function(subscription) {
-        // The subscription was successful
-        
-        // and save it to send a push message at a later date
-      console.log(subscription);
-    }).catch(function(e) {
-        if (Notification.permission === 'denied') {
-          // The user denied the notification permission which
-          // means we failed to subscribe and the user will need
-          // to manually change the notification permission to
-          // subscribe to push messages
-          console.warn('Permission for Notifications was denied');
-          // pushButton.disabled = true;
-        } else {
-          // A problem occurred with the subscription; common reasons
-          // include network errors, and lacking gcm_sender_id and/or
-          // gcm_user_visible_only in the manifest.
-          console.error('Unable to subscribe to push.', e);
-        }
-      });
-  });
-}
-
-
-self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
-  console.log(event);
-  console.log('Received a push message', event);
-  data = event.data.json();
-  var title = data.notification.title;
-  var body = data.notification.message;
-  var icon = data.notification.icon;
-  var notificationTag = data.notification.tag;
-
-  var notification = new self.Notification(title, {
-    body: body,
-    tag: notificationTag,
-    icon: icon
-  });
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: notificationTag
-    })
-  );
+const r_messaging = getMessaging(firebaseConfig);
+onMessage(r_messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
 });
 
-var BASE64_MARKER = ';base64,';
+// function subscribe() {
+//   // Disable the button so it can't be changed while
+//   // we process the permission request
+//   // var pushButton = document.querySelector('.js-push-button');
+//   // pushButton.disabled = true;
 
-function convertDataURIToBinary(dataURI) {
-  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  var base64 = dataURI.substring(base64Index);
-  var raw = window.atob(base64);
-  var rawLength = raw.length;
-  var array = new Uint8Array(new ArrayBuffer(rawLength));
+//   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+//     // const pushSubscription = serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true});
+//     // The push subscription details needed by the application
+//     // server are now available, and can be sent to it using,
+//     // for example, an XMLHttpRequest.
+//     // console.log(pushSubscription);
+//     // console.log(pushSubscription.getKey("p256dh"));
+//     // console.log(pushSubscription.getKey("auth"));
+//     const subscribeOptions = {
+//       userVisibleOnly: true,
+//       applicationServerKey: convertDataURIToBinary('BCCQ90gHgbcJsHwGMOFcA7ZleVGWn8VRvvfiQ7_kXkQjKCtbVItTZ_jWclGCWdRvWiv9wVUnVEZu6dSSwt9A13s')
+//     };
+//     serviceWorkerRegistration.pushManager.subscribe(subscribeOptions).then(function(subscription) {
+//         // The subscription was successful
+        
+//         // and save it to send a push message at a later date
+//       console.log(subscription);
+//     }).catch(function(e) {
+//         if (Notification.permission === 'denied') {
+//           // The user denied the notification permission which
+//           // means we failed to subscribe and the user will need
+//           // to manually change the notification permission to
+//           // subscribe to push messages
+//           console.warn('Permission for Notifications was denied');
+//           // pushButton.disabled = true;
+//         } else {
+//           // A problem occurred with the subscription; common reasons
+//           // include network errors, and lacking gcm_sender_id and/or
+//           // gcm_user_visible_only in the manifest.
+//           console.error('Unable to subscribe to push.', e);
+//         }
+//       });
+//   });
+// }
 
-  for(i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-  return array;
-}
+
+// self.addEventListener('push', function(event) {
+//   console.log('Received a push message', event);
+//   console.log(event);
+//   console.log('Received a push message', event);
+//   data = event.data.json();
+//   var title = data.notification.title;
+//   var body = data.notification.message;
+//   var icon = data.notification.icon;
+//   var notificationTag = data.notification.tag;
+
+//   var notification = new self.Notification(title, {
+//     body: body,
+//     tag: notificationTag,
+//     icon: icon
+//   });
+
+//   event.waitUntil(
+//     self.registration.showNotification(title, {
+//       body: body,
+//       icon: icon,
+//       tag: notificationTag
+//     })
+//   );
+// });
+
+// var BASE64_MARKER = ';base64,';
+
+// function convertDataURIToBinary(dataURI) {
+//   var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+//   var base64 = dataURI.substring(base64Index);
+//   var raw = window.atob(base64);
+//   var rawLength = raw.length;
+//   var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+//   for(i = 0; i < rawLength; i++) {
+//     array[i] = raw.charCodeAt(i);
+//   }
+//   return array;
+// }
 
 // function urlBase64ToUint8Array(base64String) {
 //   var padding = '='.repeat((4 - base64String.length % 4) % 4);
