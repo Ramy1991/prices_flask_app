1375c8b05fcb5d5b9e51fc67c6be803db7cf218c
ramy <ramygharib91@gmail.com>
2022-02-06 00:48:22 +0200

    1.2.3

--- static/js/firebase-messaging-sw.js
+++ static/js/firebase-messaging-sw.js
@@ -22,12 +22,8 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/firebase-logo.png'
-    };
-  
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
+    self.registration.showNotification(payload.data.title, {
+        body: payload.data.body,
+        icon:  payload.data.image,
+      });
 });
