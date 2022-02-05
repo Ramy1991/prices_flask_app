7bd8e8ea5c959845fa76bd31be17401fef128f6d
ramy <ramygharib91@gmail.com>
2022-02-05 23:44:28 +0200

    1.2.3

--- static/js/firebase-messaging-sw.js
+++ static/js/firebase-messaging-sw.js
@@ -18,3 +18,24 @@
 // onBackgroundMessage(messaging, (payload) => {
 //     console.log(payload);
 // });
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getMessaging } from'https://www.gstatic.com/firebasejs/9.6.6/firebase-messaging.js';;
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = getMessaging(firebaseApp);
