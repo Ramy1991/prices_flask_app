47d5c1b074f6d0c97d0f57187ca95a9d49444c4f
ramy <ramygharib91@gmail.com>
2022-02-05 22:36:35 +0200

    1.2.3

--- static/js/push_notification.js
+++ static/js/push_notification.js
@@ -111,6 +111,7 @@ self.addEventListener('push', function(event) {
   );
 });
 
+var BASE64_MARKER = ';base64,';
 
 function convertDataURIToBinary(dataURI) {
   var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
