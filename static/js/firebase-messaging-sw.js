1e7998759d4871bbc802e6fa0894b279cb1de05b
ramy <ramygharib91@gmail.com>
2022-02-06 00:05:59 +0200

    1.2.3

---
+++ static/js/firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.6/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyCQYdl0vf8gKnayNRM18dbfMpYIQxzi0LI",
+    authDomain: "bright-lattice-260000.firebaseapp.com",
+    databaseURL: "https://bright-lattice-260000.firebaseio.com",
+    projectId: "bright-lattice-260000",
+    storageBucket: "bright-lattice-260000.appspot.com",
+    messagingSenderId: "1014243229877",
+    appId: "1:1014243229877:web:71f55e2379e3c05a8bfefc",
+    measurementId: "G-275CEDTFV7"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
